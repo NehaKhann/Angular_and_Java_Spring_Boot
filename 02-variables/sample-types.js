0b2826dc746606020425691e912226e104dc3924
NehaKhann <n.nehakhan333@gmail.com>
2024-02-24 21:31:39 +0500

    first commit

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,12 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Neha";
+var lastName = 'Khan';
+//let's break it
+// found = 0;
+// grade = "A";
+// firstName = false;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hello ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,17 @@
+let found : boolean = true;
+let grade : number = 88.6;
+let firstName : string = "Neha";
+let lastName : string = 'Khan';
+
+//let's break it
+
+// found = 0;
+// grade = "A";
+// firstName = false;
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+//use of template Strings
+console.log(`Hello ${firstName} ${lastName}`);
