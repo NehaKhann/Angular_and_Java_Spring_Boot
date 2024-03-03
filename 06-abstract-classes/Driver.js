500511844bada04d1cecbc7af4dd301f8ab6f7b3
NehaKhann <n.nehakhan333@gmail.com>
2024-03-03 20:12:49 +0500

    abstract classes and interfaces

---
+++ 06-abstract-classes/Driver.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Circle_1 = require("./Circle");
+const Rectangle_1 = require("./Rectangle");
+let myCircle = new Circle_1.Circle(7, 8, 9);
+let myRectangle = new Rectangle_1.Rectangle(2, 3, 4, 6);
+let theShapes = [];
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+for (let tempShape of theShapes) {
+    console.log(tempShape.getInfo());
+    console.log(tempShape.calculateArea());
+    console.log();
+}
+// console.log(myShape.getInfo());
+// console.log(myCircle.getInfo());
+// console.log(myRectangle.getInfo());
