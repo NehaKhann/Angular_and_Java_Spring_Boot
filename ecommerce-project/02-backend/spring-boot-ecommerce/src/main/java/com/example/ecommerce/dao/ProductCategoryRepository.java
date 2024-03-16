56488b7bf32496f88d03efc0e0efdc748c4e3e47
NehaKhann <n.nehakhan333@gmail.com>
2024-03-16 23:29:46 +0500

    cross origin added

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
@@ -2,10 +2,12 @@ package com.example.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.ecommerce.entity.ProductCategory;
 //productCategory is json name (you can give any name)
 //Path name can be any
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -2,9 +2,11 @@ package com.example.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.ecommerce.entity.Product;
+import org.springframework.web.bind.annotation.*;;
 
 //Long is a Primary Key
 //Product is the entity
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
     
 }
