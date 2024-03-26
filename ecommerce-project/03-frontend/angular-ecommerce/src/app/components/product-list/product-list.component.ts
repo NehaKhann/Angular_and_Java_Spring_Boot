b338d256ac440da898a54e911df243636a06c8b4
NehaKhann <n.nehakhan333@gmail.com>
2024-03-26 20:11:24 +0500

    changes in pagination

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
 
   //new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 2;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   previousKeyword: string = '';
