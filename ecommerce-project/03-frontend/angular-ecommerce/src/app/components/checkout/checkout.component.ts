2fb7d9e9008709a600b94a19f88b228f7abfdbe2
NehaKhann <n.nehakhan333@gmail.com>
2024-04-02 22:54:16 +0500

    country and states work continuation

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { ANCommerceFormService } from 'src/app/services/ancommerce-form.service';
 
 @Component({
@@ -13,6 +15,10 @@ export class CheckoutComponent implements OnInit {
   totalQuantity: number = 0;
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+  countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
   constructor(
     private formBuilder: FormBuilder,
     private anCommerceFormService: ANCommerceFormService
@@ -63,6 +69,12 @@ export class CheckoutComponent implements OnInit {
       console.log('Retrieved Credit Card Years: ' + JSON.stringify(data));
       this.creditCardYears = data;
     });
+
+    // populate countries
+    this.anCommerceFormService.getCountries().subscribe((data) => {
+      console.log('Retrieved countries: ' + JSON.stringify(data));
+      this.countries = data;
+    });
   }
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
@@ -76,6 +88,14 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log(
+      'The shipping address country is ' +
+        this.checkoutFormGroup.get('shippingAddress')?.value.country.name
+    );
+    console.log(
+      'The shipping address state is ' +
+        this.checkoutFormGroup.get('shippingAddress')?.value.state.name
+    );
   }
 
   handleMonthsAndYears() {
@@ -98,5 +118,26 @@ export class CheckoutComponent implements OnInit {
         console.log('Retrieved credit card months: ' + JSON.stringify(data));
         this.creditCardMonths = data;
       });
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.anCommerceFormService.getStates(countryCode).subscribe((data) => {
+      if (formGroupName === 'shippingAddress') {
+        this.shippingAddressStates = data;
+      } else {
+        this.billingAddressStates = data;
+      }
+
+      // select first item by default
+      formGroup?.get('state')?.setValue(data[0]);
+    });
   }
 }
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/ancommerce-form.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/ancommerce-form.service.ts
@@ -1,11 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ANCommerceFormService {
-  constructor() {}
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) {}
+
+  // Here, this.httpClient.get() makes an HTTP GET request to a specific
+  // URL (this.countriesUrl). The response
+  // from the server is expected to be of type GetResponseCountries
+  getCountries(): Observable<Country[]> {
+    return this.httpClient
+      .get<GetResponseCountries>(this.countriesUrl)
+      .pipe(map((response) => response._embedded.countries));
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    //search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+    return this.httpClient
+      .get<GetResponseStates>(searchStatesUrl)
+      .pipe(map((response) => response._embedded.states));
+  }
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let data: number[] = [];
 
@@ -28,4 +52,15 @@ export class ANCommerceFormService {
     }
     return of(data);
   }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  };
+}
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  };
 }
