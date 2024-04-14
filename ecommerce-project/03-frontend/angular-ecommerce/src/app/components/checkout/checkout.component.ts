f0005dbeea3bf1d8e970c041153412aa8f746fad
NehaKhann <n.nehakhan333@gmail.com>
2024-04-14 20:24:34 +0500

    order history functionality added

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -35,6 +35,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage : Storage = sessionStorage;
+
   constructor(
     private formBuilder: FormBuilder,
     private anCommerceFormService: ANCommerceFormService,
@@ -46,6 +48,8 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.reviewCartDetails();
 
+    //read uers email address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('', [
@@ -58,7 +62,7 @@ export class CheckoutComponent implements OnInit {
           Validators.minLength(2),
           ANCommerceValidators.notOnlyWhitespace,
         ]),
-        email: new FormControl('', [
+        email: new FormControl(theEmail, [
           Validators.required,
           Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
           ANCommerceValidators.notOnlyWhitespace,
