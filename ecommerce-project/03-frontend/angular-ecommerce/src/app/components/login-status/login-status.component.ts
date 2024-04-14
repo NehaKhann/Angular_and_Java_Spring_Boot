79b2315d3ecffdee3e5a9d58ef5e0846c5437172
NehaKhann <n.nehakhan333@gmail.com>
2024-04-14 17:33:03 +0500

    order history service added

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -10,6 +10,9 @@ import { OktaAuth } from '@okta/okta-auth-js';
 export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = '';
+
+  storage: Storage = sessionStorage;
+
   constructor(
     private oktaAuthService: OktaAuthStateService,
     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
@@ -29,6 +32,11 @@ export class LoginStatusComponent implements OnInit {
       //user full name is exposed as a property name
       this.oktaAuth.getUser().then((res) => {
         this.userFullName = res.name as string;
+        //retrieve the user's email from authentication response
+        const theEmail = res.email;
+
+        //now store the email in browser storage
+        this.storage.setItem('userEmail', JSON.stringify(theEmail));
       });
     }
   }
